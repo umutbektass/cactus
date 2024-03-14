85ac91ed2d5dfc12d3d96eeffe207416004ee136
umutbektass <umutbektass.78@hotmail.com>
2024-03-14 23:34:30 +0300

    cactus price added

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,11 @@ const linkAction = ()=>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(link=>link.addEventListener('click',linkAction))
+
+const blurHeader = ()=>{
+    const header = document.querySelector('.header');
+    this.scrollY >= 50 ? header.classList.add('blur-header') : 
+    header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll',blurHeader)
