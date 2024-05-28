6f7b3a78606d6c6bf7a0f6baa82a48eeecd8ed2e
Umut Bektaş <umutbektass.78@hotmail.com>
2024-05-28 17:12:39 +0300

    contact and footer sections added, styles created

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,3 +23,12 @@ const blurHeader = ()=>{
 }
 
 window.addEventListener('scroll',blurHeader)
+
+        /*=============== SHOW SCROLL UP ===============*/
+    const scrollUp = ()=>{
+        const scrollUp = document.getElementById('scroll-up');
+
+        this.scrollY>=350 ? scrollUp.classList.add('show-scroll') : 
+        scrollUp.classList.remove('show-scroll')
+    }
+    window.addEventListener('scroll',scrollUp)
