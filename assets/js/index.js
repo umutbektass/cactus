1e9aff8fb033a58d91c73c42d9081b7b617da89d
Umut Bektaş <umutbektass.78@hotmail.com>
2024-05-29 17:41:33 +0300

    - Added ScrollReveal animations to elements - Improved responsive design for various screen sizes - Changed background color of mobile navigation menu for better visibility

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,3 +32,37 @@ window.addEventListener('scroll',blurHeader)
         scrollUp.classList.remove('show-scroll')
     }
     window.addEventListener('scroll',scrollUp)
+
+         /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+      const sections = document.querySelectorAll('section[id]')
+      
+      const scrollActive = ()=>{
+        const scrollDown = window.scrollY
+
+        sections.forEach(current=>{
+            const sectionHeight = current.offsetHeight,
+                    sectionTop = current.offsetTop-58,
+                    sectionId  = current.getAttribute('id'),
+                    sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+                    if(scrollDown> sectionTop && scrollDown<=sectionTop + sectionHeight){
+                        sectionClass.classList.add('active-link')
+                    }
+                    else{
+                        sectionClass.classList.remove('active-link')
+                    }
+        })
+      }
+      window.addEventListener('scroll',scrollActive)
+
+      const sr = ScrollReveal({
+        origin:'top',
+        distance:'80px',
+        duration:750,
+        delay:300
+      })
+
+      sr.reveal(`.home__img, .new__data, .care__img, .contact__content, .footer`)
+      sr.reveal(`.home__data, .care__list, .contact__img`,{delay:500})
+      sr.reveal(`.new__card`,{delay:500, interval:100})
+      sr.reveal(`.shop__card`,{interval:100})
+
